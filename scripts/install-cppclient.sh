fd81c68b28dfb81eb428450c40bef315ddb59223
liuyijie <lyjhust@hust.edu.cn>
2023-03-29 11:16:21 +0800

    bugfix: scripts/install-cppclient.sh

--- scripts/install-cppclient.sh
+++ scripts/install-cppclient.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ARGS="-it --rm -v $(realpath cppclient):/opt/jiffy/cppclient --entrypoint cp"
+cmd_dir="-r /opt/jiffy/build/external/thrift_ep/lib /opt/jiffy/cppclient/"
 cmd_cli="/usr/local/lib/libjiffy_client.a /opt/jiffy/cppclient/lib/"
-cmd_dir="/opt/jiffy/build/external/thrift_ep/lib/libthrift.a /opt/jiffy/build/external/thrift_ep/lib/libthriftnb.a /opt/jiffy/cppclient/lib/"
 
-docker run --name jiffy-cppclient-client $ARGS jiffy-client:0.1.0 $cmd_cli
 docker run --name jiffy-cppclient-directory $ARGS jiffy-directory:0.1.0 $cmd_dir
+docker run --name jiffy-cppclient-client $ARGS jiffy-client:0.1.0 $cmd_cli
